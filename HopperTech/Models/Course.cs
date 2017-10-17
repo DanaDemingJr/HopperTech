ea30669efd89e5ba56dc98a9a0e270cd0c294193
ddemingjr22 <ddemingjr22@yahoo.com>
2017-10-16 20:43:18 -0400

    first commit

---
+++ HopperTech/Models/Course.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace HopperTech.Models
+{
+    public class Course
+    {
+        [Key]
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int CourseHours { get; set; }
+        public string Room { get; set; }
+        public string Department { get; set; }
+
+    }
+}
---
+++ HopperTech/Models/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace HopperTech.Models
+{
+    public class Student
+    {
+        [Key]
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public DateTime DOB { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+        public string HomeAddress { get; set; }
+        public string Email { get; set; }
+
+        [ForeignKey("Course")]
+        public int CourseID { get; set; }
+        public virtual Course Course { get; set; }
+
+
+    }
+}
